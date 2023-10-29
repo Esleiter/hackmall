0793627c70f3a6e8c60ab7dd2fc3e7143c35c4c0
esleiter <ceo@esleiter.com>
2023-10-29 00:36:06 -0500

    modifie amount

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -31,8 +31,6 @@ const Cart = () => {
   }, [totalAmt]);
 
   useEffect(() => {
-    const uniqueId = Math.random().toString(36).substr(2, 9); // Puedes usar otra lógica para generar un identificador único
-    setClientTransactionId(uniqueId);
     // Agrega el script de Payphone directamente
     const script = document.createElement("script");
     script.text = `
@@ -49,7 +47,7 @@ const Cart = () => {
               amount: 100,
               amountWithoutTax: 100,
               currency: "USD",
-              clientTransactionId: "${uniqueId}",
+              clientTransactionId: "00000000004",
               lang: "es"
             }).then(function(paramlog){
               console.log(paramlog);
